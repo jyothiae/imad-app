be2fdbbf6e4fb67b4ba780bdf6ab0c8f33190237
Jyothi.A <jyothiae96@yahoo.in>
2018-02-16 19:08:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var articles={
      'article-two':{
         title:'article-two by jyothi',
         dateAndTime:'16-02-2018 17:33',
-        heading:'article-three',
+        heading:'article-two',
         content:`<p>
             this is artical two  paragraph. blah blah blah!!!!
         </p>
